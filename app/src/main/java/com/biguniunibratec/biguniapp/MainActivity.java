f213da27164713d2c7ed54f83d47d02c57e0a73d
LAPTOP-F2B1CM1M\Rayssa&Thiago <rayssa.nog.s@hotmail.com>
2017-04-07 08:48:27 -0300

    testando tela

---
+++ app/src/main/java/com/biguniunibratec/biguniapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.biguniunibratec.biguniapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
